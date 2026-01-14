0634992d190c3be40486a2a52634cbce18475663
Andrey Badanov <166286455+Andrey-Badanow@users.noreply.github.com>
2026-01-14 22:52:16 +0300

    Implement UART initialization and update clock setup
    Added UART initialization function and updated peripheral clock setup.

--- STM32F411 Transmitter/main.c
+++ STM32F411 Transmitter/main.c
@@ -5,13 +5,15 @@
 void set_96_hse(void);
 void led_c13_init(void);
 void set_peripheral_clock(void);
+void uart_init(void);
 
 int main(void)
 {
     set_peripheral_clock();
-		led_c13_init();
-    set_96_hse();
+	set_96_hse();
+	led_c13_init();
     LED_C13_ON();
+		
     while (1){
     }
 }
@@ -46,9 +48,9 @@ void set_96_hse(void)
 
 void set_peripheral_clock(void)
 {
-    RCC->AHB1ENR |=
-        (RCC_AHB1ENR_GPIOCEN |
-         RCC_AHB1ENR_GPIOBEN);
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; // led_c13
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // uart gpio
+		RCC->APB2ENR |= RCC_APB2ENR_USART1EN; //uart
 }
 
 void led_c13_init(void)
@@ -56,6 +58,30 @@ void led_c13_init(void)
     GPIOC->MODER |= GPIO_MODER_MODER13_0;
     GPIOC->MODER &= ~GPIO_MODER_MODER13_1;
     GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;
-    GPIOC->OSPEEDR &=! ~GPIO_OSPEEDR_OSPEED13_0;
-		GPIOC->OSPEEDR &=! ~GPIO_OSPEEDR_OSPEED13_1;
+    GPIOC->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED13_0;
+	GPIOC->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED13_1;
+}
+
+void uart_init(void)
+{
+	GPIOA->MODER &= ~GPIO_MODER_MODER9;
+	GPIOA->MODER |= GPIO_MODER_MODER9_1;
+	GPIOA->MODER &= ~GPIO_MODER_MODER10;
+	GPIOA->MODER |= GPIO_MODER_MODER10_1;
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD10; 
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPD10_0;    
+	GPIOA->AFR[1] |= (7 << GPIO_AFRH_AFSEL9_Pos) |
+    	           (7 << GPIO_AFRH_AFSEL10_Pos);
+	/*
+	92 000 000 / 115200 = 833,333
+	833,33 / 16 = 52,0833
+	mant = 52
+	frac = 0,0833 * 16 = 1,33 ~ 1
+	*/
+	USART1->BRR = (52 << 4) | (1 << 0); 
+	USART1->CR1 |= 
+			(1 << USART_CR1_RE_Pos) |
+			(1 << USART_CR1_TE_Pos) |
+			(1 << USART_CR1_RXNEIE_Pos) |
+			(1 << USART_CR1_UE_Pos);
 }
